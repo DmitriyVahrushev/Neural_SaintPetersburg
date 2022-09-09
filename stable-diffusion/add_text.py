32fbfb5cde6f41a103d3027006b5d5ce9e761f6d
talveRinat <rrinatmahmutt@icloud.com>
2022-09-09 15:47:07 +0300

    fix add_text.py

--- stable-diffusion/add_text.py
+++ stable-diffusion/add_text.py
@@ -1,22 +1,44 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
+def break_text(txt: str, font, max_width: int):
+
+    # We share the subset to remember the last finest guess over
+    # the text breakpoint and make it faster
+    subset: int = len(txt)
+
+    text_size = len(txt)
+    while text_size > 0:
+
+        # Let's find the appropriate subset size
+        while True:
+            width, height = font.getsize(txt[:subset])
+            letter_size: float = width / subset
+
+            # min/max(..., subset +/- 1) are to avoid looping infinitely over a wrong value
+            if width < max_width - letter_size and text_size >= subset:  # Too short
+                subset = max(int(max_width * subset / width), subset + 1)
+            elif width > max_width:  # Too large
+                subset = min(int(max_width * subset / width), subset - 1)
+            else:  # Subset fits, we exit
+                break
+
+        yield txt[:subset]
+        txt = txt[subset:]
+        text_size = len(txt)
+
+
 def add_txt(title: str, img_path: str):
     """Adding text on image"""
 
     img = Image.open(img_path, "r")
-    img_width, img_height = img.size
+    img_width, _ = img.size
 
     drawing = ImageDraw.Draw(img)
 
-    font = ImageFont.truetype("font/font.ttf", 30)  # шрифт и размера
-    text_width, text_height = drawing.textsize(title, font)
+    font = ImageFont.truetype("font/font.ttf", 20)  # шрифт и размера
 
-    pos = (
-        (img_width - text_width) // 2,
-        (img_height - text_height),
-    )
-
-    drawing.text(pos, title, font=font, fill="white")
+    for i, line in enumerate(break_text(title, font, img_width)):
+        drawing.text((0, 22 * i), line, font=font, fill="white")
 
     return img
